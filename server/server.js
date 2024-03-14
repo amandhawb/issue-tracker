dcb6361638a595f09ce81082f3519bceb70d7853
Amandha W. Barok <amandha.barok@gmail.com>
2024-03-14 10:24:48 -0400

    Modifing the server port to 3001 and adding CORS

--- server/server.js
+++ server/server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+// Middleware to handle CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // Initial hardcoded data
 let issues = [
