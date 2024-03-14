8d0af3b41d25bd0f17e541664ce3019e7db55a39
Amandha W. Barok <amandha.barok@gmail.com>
2024-03-14 14:30:39 -0400

    last changes

--- client/src/components/IssueList.js
+++ client/src/components/IssueList.js
@@ -5,7 +5,6 @@ function getRandomNumber() {
     return Math.round(Math.random() * 500);
 }
 
-
 function IssueList() {
     const [issues, setIssues] = useState([]);
 
@@ -35,16 +34,42 @@ function IssueList() {
         }
     };
 
+    const handleDeleteIssue = async (id) => {
+        try {
+            await axios.delete(`http://localhost:3001/issues/${id}`);
+            fetchIssues();
+        } catch (error) {
+            console.error('Error deleting issue:', error);
+        }
+    };
+
+    const handleUpdateIssue = async (id) => {
+        try {
+            await axios.put(`http://localhost:3001/issues/${id}`, { description: "Descriptio updated" });
+            fetchIssues();
+        } catch (error) {
+            console.error('Error updating', error);
+        }
+    };
+
+    const handleOpenIssue = (link) => {
+        window.open(link, '_blank');
+    };
+
     return (
         <div>
             <h1>Issue List</h1>
             <button className="add-issue-button" onClick={handleCreateIssue}>Create Issue</button>
             <ul>
                 {issues.map(issue => (
-                    <li key={issue.id}>
+                    <li key={issue.id}>       
                         <h2>{issue.title}</h2>
                         <p>{issue.description}</p>
                         <a href={issue.link}>{issue.link}</a>
+                        <button onClick={() => handleDeleteIssue(issue.id)}>Delete Issue</button>
+                        <button onClick={() => handleUpdateIssue(issue.id)}>Update Issue</button>
+                        <button onClick={() => handleOpenIssue(issue.link)}>Open Issue</button>
+
                     </li>
                 ))}
             </ul>
